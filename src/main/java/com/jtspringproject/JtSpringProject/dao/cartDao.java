452674845cb232ac646a73b6ff2d3ce0559bb271
Jagadeesh Tanniru <jagadeesh.tanniru@serole.com>
2025-01-28 13:00:07 +0530

    changes made

--- src/main/java/com/jtspringproject/JtSpringProject/dao/cartDao.java
+++ src/main/java/com/jtspringproject/JtSpringProject/dao/cartDao.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-public class cartDao {
+public class  cartDao {
     @Autowired
     private SessionFactory sessionFactory;
 
